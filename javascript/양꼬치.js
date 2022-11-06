1b87a857a9d1f6fc3dd0ede00dba13fe6a12c079
jinnkimm7 <jinnkimm7@gmail.com>
2022-11-06 22:06:18 +0900

    Create 양꼬치.js

---
+++ javascript/양꼬치.js
@@ -0,0 +1,5 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120830?language=javascript 
+
+function solution(n, k) {
+  return (12000 * n) + (2000 * k) - (Math.floor(n / 10) * 2000);
+}
