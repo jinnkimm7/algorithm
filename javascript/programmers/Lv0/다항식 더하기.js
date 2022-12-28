1631baf3a8c9315d16b325677d55151f4c9bcf49
jinnkimm7 <jinnkimm7@gmail.com>
2022-12-28 23:26:44 +0900

    Update 다항식 더하기.js

--- javascript/programmers/Lv0/다항식 더하기.js
+++ javascript/programmers/Lv0/다항식 더하기.js
@@ -1,22 +1,28 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120863
 
 function solution(polynomial) {
-  var answer = '';
-
-  const arr = polynomial.split(' ');
-
-  const x = [];
+  polynomial = polynomial.split('+').map(v => v.trim());
+  const xs = [];
   const numbers = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i].includes('x')) x.push(arr[i]);
-    else if (parseInt(arr[i])) numbers.push(arr[i]);
+  for (let i = 0; i < polynomial.length; i++) {
+    if (polynomial[i].includes('x')) xs.push(polynomial[i]);
+    else numbers.push(polynomial[i]);
   }
 
-  x.map(v => v.splice('x', ''));
+  const x = xs.map(v => v.replace('x', ''))
+    .reduce((a, c) => {
+      if (c === '') return a + 1;
+      else return a + (c * 1)
+    }, 0);
 
-  return x;
+  const number = numbers.reduce((a, c) => a + (c * 1), 0);
+
+  if (number === 0 && x === 0) return 0;
+  if (x === 1 && number !== 0) return `x + ${number}`;
+  if (x === 1) return `x`;
+  if (x === 0) return `${number}`;
+  if (number === 0) return `${x}x`;
+
+  return `${x}x + ${number}`;
 }
-
-console.log(solution("3x + 7 + x"));
-console.log(solution("x + x + x"));
