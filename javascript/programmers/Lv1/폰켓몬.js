d9ca09f9918702a5901311afcb6368157b04ccd9
jinnkimm7 <jinnkimm7@gmail.com>
2023-01-01 23:33:03 +0900

    Create 폰켓몬.js

---
+++ javascript/programmers/Lv1/폰켓몬.js
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/1845
+
+function solution(nums) {
+  const max = nums.length / 2;
+  const arr = [...new Set(nums)];
+
+  return arr.length > max ? max : arr.length;
+}
