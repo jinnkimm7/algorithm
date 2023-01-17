cd6ba29a678288fc014b0443b039a5ecb9179343
Jin <jinnkimm7@gmail.com>
2023-01-18 00:04:19 +0900

    Update 구명보트.js

--- javascript/programmers/Lv2/구명보트.js
+++ javascript/programmers/Lv2/구명보트.js
@@ -1,10 +1,32 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=javascript
 
+// 첫 풀이 30/100
 function solution(people, limit) {
-  var answer = 0;
+  let answer = 0;
+
   people = people.sort((a, b) => b - a);
 
-  for (var i = 0, j = people.length - 1; i <= j; i++, answer++) {
+  while (people.length !== 0) {
+    let temp = limit;
+    let person = people.pop();
+    temp = temp - person;
+    person = people.pop();
+    temp = temp - person;
+
+    if (temp < 0) {
+      people.push(person);
+      answer++;
+    } else answer++;
+  }
+
+  return answer;
+}
+
+function solution2(people, limit) {
+  let answer = 0;
+  people = people.sort((a, b) => b - a);
+
+  for (let i = 0, j = people.length - 1; i <= j; i++, answer++) {
     if (people[i] + people[j] <= limit) j--;
   }
 
