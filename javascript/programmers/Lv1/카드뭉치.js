893ba16567f65f03432f974e690eea5567238715
Jin <jinnkimm7@gmail.com>
2023-02-18 00:22:56 +0900

    Update 카드뭉치.js

--- javascript/programmers/Lv1/카드뭉치.js
+++ javascript/programmers/Lv1/카드뭉치.js
@@ -1,21 +1,25 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/159994
 
-function solution1(cards) {
-  while (cards.length > 1) {
-    // 정렬
-    cards.sort((a, b) => a - b)
-    battle(cards)
+// 48/100
+function solution(cards1, cards2, goal) {
+  let goalDoubleArr = [];
+  for (let i = 0; i < goal.length; i += 2) {
+    goalDoubleArr.push(goal.slice(i, i + 2));
   }
 
-  return cards.length ? cards[0] : 0
-}
+  const minLen = Math.min(cards1.length, cards2.length);
+  const maxLen = Math.max(cards1.length, cards2.length);
 
-function battle(cards) {
-  const life1 = cards.pop()
-  const life2 = cards.pop()
+  let cards = [];
+  for (let i = 0; i < minLen; i++) {
+    cards.push([cards1[i], cards2[i]]);
+  }
+  if (cards1[maxLen - 1]) cards.push([cards1[maxLen - 1]]);
+  if (cards2[maxLen - 1]) cards.push([cards2[maxLen - 1]]);
 
-  const leftLife = Math.abs(life1 - life2)
+  goalDoubleArr = (goalDoubleArr.map(v => v.sort())).map(v => v.join('')).join('');
+  cards = (cards.map(v => v.sort())).map(v => v.join('')).join('');
 
-  // 남은 카드 추가
-  if (leftLife) cards.push(leftLife)
+  if (goalDoubleArr === cards) return 'Yes';
+  return 'No';
 }
