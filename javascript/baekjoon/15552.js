985e9b861d02dd9756157f97a50481407ebbe8e1
Jin <jinnkimm7@gmail.com>
2023-05-16 23:30:39 +0900

    Update 15552.js

--- javascript/baekjoon/15552.js
+++ javascript/baekjoon/15552.js
@@ -1,2 +1,15 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const number = parseInt(input[0]);
+let answer = '';
+
+for (let i = 1; i <= number; i++) {
+  const data = input[i].split(' ').map(v => parseInt(v));
+  const a = data[0];
+  const b = data[1];
+
+  answer += a + b + '\n';
+}
+
+console.log(answer);
