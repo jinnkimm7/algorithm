d397c680cb4261ddba44bd11a20199c8bdb69de5
Jin <jinnkimm7@gmail.com>
2023-05-29 18:46:07 +0900

    Update 2606.js
    두 번째 풀기

--- javascript/baekjoon/2606.js
+++ javascript/baekjoon/2606.js
@@ -1,26 +1,29 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-// n = 컴퓨터의 수, m = 연결된 수
-const n = Number(input[0]);
-const m = Number(input[1]);
+const n = Number(input[0]); // 컴퓨터의 수
+const m = Number(input[1]); // 연결되어 있는 컴퓨터의 수 
 
 const graph = [];
-for (let i = 1; i <= n; i++) graph[i] = [];
+for (let i = 0; i <= n; i++) graph[i] = [];
 for (let i = 2; i <= m + 1; i++) {
   const [x, y] = input[i].split(' ').map(v => Number(v));
   graph[x].push(y);
   graph[y].push(x);
 }
 
+let answer = 0;
+
 const visited = new Array(n + 1).fill(false);
-let count = 0;
 
 function dfs(x) {
   visited[x] = true;
-  count++;
-  for (y of graph[x]) if (!visited[y]) dfs(y);
+  answer++;
+
+  for (y of graph[x]) {
+    if (!visited[y]) dfs(y);
+  }
 }
 
 dfs(1);
-console.log(count - 1);
+console.log(answer - 1);
