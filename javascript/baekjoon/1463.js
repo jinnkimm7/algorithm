5e333b3895160c6981c9bbe97d2415c7caa5038a
Jin <jinnkimm7@gmail.com>
2023-09-11 23:48:04 +0900

    DFS, DP 한 문제씩 복습

--- javascript/baekjoon/1463.js
+++ javascript/baekjoon/1463.js
@@ -2,18 +2,18 @@ const fs = require('fs');
 const input = fs.readFileSync('/Users/jin/Documents/Study/algorithm/javascript/baekjoon/input.txt').toString().split('\n');
 
 const n = Number(input[0]);
-const dp = [0, 0, ...new Array(n).fill(0)];
+const dp = [0, 0];
 
 for (let i = 2; i <= n; i++) {
-  dp[i] = dp[i - 1] + 1;
-
-  if (i % 2 === 0) {
-    dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
-  }
-
-  if (i % 3 === 0) {
-    dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+  if (i % 2 === 0 & i % 3 === 0) { // 2로도 나누어 떨어지고, 3으로도 나누어 떨어지는 경우,
+    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2] + 1, dp[i / 3] + 1);
+  } else if (i % 2 === 0) { // 2로 나누어 떨어지는 경우,
+    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2] + 1);
+  } else if (i % 3 === 0) { // 3으로 나누어 떨어지는 경우,
+    dp[i] = Math.min(dp[i - 1] + 1, dp[i / 3] + 1);
+  } else {
+    dp[i] = dp[i - 1] + 1;
   }
 }
-
+// console.log(dp);
 console.log(dp[n]);
--- javascript/baekjoon/2178.js
+++ javascript/baekjoon/2178.js
@@ -2,25 +2,29 @@ const fs = require('fs');
 const input = fs.readFileSync('/Users/jin/Documents/Study/algorithm/javascript/baekjoon/input.txt').toString().split('\n');
 
 const [n, m] = input[0].split(' ').map(Number);
+
 const graph = [];
 for (let i = 1; i <= n; i++) {
   graph.push(input[i].split('').map(Number));
 }
 
 function bfs(startX, startY) {
-  const dx = [1, -1, 0, 0];
-  const dy = [0, 0, 1, -1];
-
   const queue = [];
   queue.push([startX, startY]);
+
+  const dx = [-1, 1, 0, 0];
+  const dy = [0, 0, -1, 1];
 
   while (queue.length !== 0) {
     const [x, y] = queue.shift();
 
     for (let i = 0; i < 4; i++) {
       const [nx, ny] = [x + dx[i], y + dy[i]];
+      // 범위를 벗어나는 경우,
       if (nx >= n || nx <= -1 || ny >= m || ny <= -1) continue;
+      // 벽인 경우,
       if (graph[nx][ny] === 0) continue;
+      // 갈 수 있는 경우
       if (graph[nx][ny] === 1) {
         graph[nx][ny] = graph[x][y] + 1;
         queue.push([nx, ny]);
