43ab7a15f7f35bbbab190b4e00f8d0384524573c
Jin <jinnkimm7@gmail.com>
2023-10-02 22:03:34 +0900

    미로찾기(BFS) 다시 풀기

--- javascript/baekjoon/2178.js
+++ javascript/baekjoon/2178.js
@@ -1,30 +1,29 @@
 const fs = require('fs');
 const input = fs.readFileSync('/Users/jin/Documents/Study/algorithm/javascript/baekjoon/input.txt').toString().split('\n');
 
-const [n, m] = input[0].split(' ').map(Number);
+const [N, M] = input[0].split(' ').map(Number);
 
 const graph = [];
-for (let i = 1; i <= n; i++) {
-  graph.push(input[i].split('').map(Number));
-}
+for (let i = 1; i <= N; i++) graph.push(input[i].split('').map(Number));
 
-function bfs(startX, startY) {
-  const queue = [];
-  queue.push([startX, startY]);
+function bfs() {
+  const queue = [[0, 0]];
 
-  const dx = [-1, 1, 0, 0];
-  const dy = [0, 0, -1, 1];
+  // 이동할 수 있는 방향: 상하좌우
+  const dx = [0, 0, -1, 1];
+  const dy = [1, -1, 0, 0];
 
   while (queue.length !== 0) {
     const [x, y] = queue.shift();
 
     for (let i = 0; i < 4; i++) {
       const [nx, ny] = [x + dx[i], y + dy[i]];
-      // 범위를 벗어나는 경우,
-      if (nx >= n || nx <= -1 || ny >= m || ny <= -1) continue;
-      // 벽인 경우,
+
+      // 1. 범위를 벗어난 경우, pass
+      if (nx >= N || nx <= -1 || ny >= M || ny <= -1) continue;
+      // 2. 0(벽)인 경우, pass
       if (graph[nx][ny] === 0) continue;
-      // 갈 수 있는 경우
+
       if (graph[nx][ny] === 1) {
         graph[nx][ny] = graph[x][y] + 1;
         queue.push([nx, ny]);
@@ -33,5 +32,5 @@ function bfs(startX, startY) {
   }
 }
 
-bfs(0, 0);
-console.log(graph[n - 1][m - 1]);
+bfs();
+console.log(graph[N - 1][M - 1]);
