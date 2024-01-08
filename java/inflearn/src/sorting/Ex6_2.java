f25936349bfaa064f81709d6c6df0aa0cdb61975
Jin <jinnkimm7@gmail.com>
2024-01-08 23:39:24 +0900

    Sorting

---
+++ java/inflearn/src/sorting/Ex6_2.java
@@ -0,0 +1,32 @@
+package sorting;
+
+import java.util.Scanner;
+
+public class Ex6_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Ex6_2 main = new Ex6_2();
+        for(int num : main.solution(arr)){
+            System.out.print(num + " ");
+        };
+    }
+
+    public int[] solution(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
