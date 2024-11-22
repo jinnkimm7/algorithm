3ac386790dcc3124140aeb3ab4561aa68aefcac0
Jin <93902029+jinnkimm7@users.noreply.github.com>
2024-11-23 00:48:21 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+    
+        int idx = 0;
+        for(int[] command : commands) {
+            int[] tmp = Arrays.copyOfRange(array, command[0] - 1, command[1]);
+            Arrays.sort(tmp);
+            
+            answer[idx++] = tmp[command[2] - 1];
+        }
+        
+        return answer;
+    }
+}
