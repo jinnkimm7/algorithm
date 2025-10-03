9e154a19653e06ded990f10ce175e7fcf1e05581
Jin <93902029+jinnkimm7@users.noreply.github.com>
2025-10-03 21:49:41 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.76 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -3,10 +3,20 @@ function solution(participant, completion) {
     
     const map = new Map();
     
-    participant.forEach(p => map.set(p, (map.get(p) || 0) + 1));
-    completion.forEach(c => map.set(c, map.get(c) - 1));
-    
-    for (const [key, value] of map) {
-        if (value > 0) return key;
+    for (let i = 0; i < participant.length; i++) {
+        if (map.has(participant[i])) {
+            map.set(participant[i], map.get(participant[i]) + 1);
+        }
+        else map.set(participant[i], 1);
     }
+    
+    for (let i = 0; i < completion.length; i++) {
+        if (map.has(completion[i])) map.set(completion[i], map.get(completion[i]) - 1);
+    }
+    
+    for (let [key, value] of map) {
+        if (value > 0) answer = key;
+    }
+    
+    return answer;
 }
